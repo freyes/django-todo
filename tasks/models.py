a27f4bf955fb26ce41b3a7db58c010a8fe57735a
Aaron Reisman <aaron@seatme.com>
2012-07-15 19:05:57 -0700

    unit tests for Task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
-# Create your models here.
-# class Task(models.Model):
+class Task(models.Model):
 
-# 	completed = models.BooleanField(default=False)
-# 	description = models.CharField(max_length=255)
-
-# 	def __init__(self):
-# 		return self.description
+ 	completed = models.BooleanField(default=False)
+ 	description = models.CharField(max_length=255)
--- tasks/tests.py
+++ tasks/tests.py
@@ -6,7 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from models import Task
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -16,3 +16,16 @@ class SimpleTest(TestCase):
         self.assertEqual(1 + 1, 2)
 
 
+class TaskTest(TestCase):
+	def test_completed_should_be_false(self):
+		"""
+		Tests that completed is false.
+		"""
+		task = Task()
+		self.assertEqual(task.completed, False)
+	def test_description_should_allow_blank(self):
+		"""
+		Description field should allow blank value.
+		"""
+		task = Task()
+		self.assertEqual(task.description, '')
