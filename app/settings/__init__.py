6e8c70b25632296544ce4cf098d7be6c0e7d9450
Aaron Reisman <aaron@seatme.com>
2012-07-15 18:40:36 -0700

    fixing settings

--- app/settings/__init__.py
+++ app/settings/__init__.py
@@ -4,7 +4,8 @@ from app.settings.development import *
 ENV_NAME = ''
 if os.environ.has_key("ENV_NAME"):
     try:
-        exec "from app.settings.%s import *" % os.environ["ENV_NAME"]
         ENV_NAME = os.environ["ENV_NAME"]
+        exec "from app.settings.%s import *" % ENV_NAME
     except ImportError, exp:
         pass
+ENV_NAME = 'development'
