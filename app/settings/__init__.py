e9c1fd506bde217aa93b05d58c95e5a1f5a16147
Aaron Reisman <aaron@seatme.com>
2012-07-15 18:45:50 -0700

    Fixing settings for the last fucking time

--- app/settings/__init__.py
+++ app/settings/__init__.py
@@ -4,7 +4,7 @@ from app.settings.development import *
 ENV_NAME = ''
 if os.environ.has_key("ENV_NAME"):
     try:
-        ENV_NAME = os.environ["ENV_NAME"]
+        ENV_NAME = os.environ.get("ENV_NAME")
         exec "from app.settings.%s import *" % ENV_NAME
     except ImportError, exp:
         pass
