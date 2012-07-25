1b35280d390898f491523512a1e97c8aad43deba
Aaron Reisman <aaron@seatme.com>
2012-07-24 22:24:59 -0700

    view(controller) has logic for creating task

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,17 @@
 # Create your views here.
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from forms import TaskForm
 
 
 def index(request):
-	form = TaskForm()
+	if request.method == 'POST':
+		form = TaskForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('index'))
+	else:
+		form = TaskForm()
+
 	return render(request, 'tasks/index.html', {"name": "World", "form": form})
