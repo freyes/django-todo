e8a50be166808cf1d161545984c641b40ac7785a
Aaron Reisman <aaron@seatme.com>
2012-07-24 20:35:15 -0700

    test: Task can be fetched

--- tasks/tests.py
+++ tasks/tests.py
@@ -3,15 +3,26 @@ from models import Task
 
 
 class TaskTest(TestCase):
-	def test_completed_should_be_false(self):
-		"""
-		Tests that completed is false.
-		"""
-		task = Task()
-		self.assertEqual(task.completed, False)
-	def test_description_should_allow_blank(self):
-		"""
-		Description field should allow blank value.
-		"""
-		task = Task()
-		self.assertEqual(task.description, '')
+    def test_completed_should_be_false(self):
+        """
+        Tests that completed is false.
+        """
+        task = Task()
+        self.assertEqual(task.completed, False)
+
+    def test_description_should_allow_blank(self):
+        """
+        Description field should allow blank value.
+        """
+        task = Task()
+        self.assertEqual(task.description, '')
+
+    def test_task_should_be_fetchable(self):
+        """
+        A Task should be fetchable via the ORM
+        """
+        task = Task(description="Bob")
+        task.save()
+        fetched_task = Task.objects.filter(description="Bob")[0]
+
+        self.assertEqual(fetched_task.id, task.id)
