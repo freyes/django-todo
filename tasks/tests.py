94e7d51537c5390dd8470de0b0d0d3459dec4da0
Aaron Reisman <aaron@seatme.com>
2012-07-24 20:14:46 -0700

    deleting bs tests

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,19 +1,5 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 from models import Task
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
 
 
 class TaskTest(TestCase):
