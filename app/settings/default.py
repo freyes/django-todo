436ee4033f170b87301716518c5e58e3968d10d9
Aaron Reisman <aaron@seatme.com>
2012-07-31 21:42:45 -0700

    Delete a task through the browser

--- app/settings/default.py
+++ app/settings/default.py
@@ -83,9 +83,11 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # @todo csrf breaks delete
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'respite.middleware.HttpMethodOverrideMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
