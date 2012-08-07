4036fc60db83a4c3d04c632e81e70a64ff84eea4
Grayson Koonce <grayson.koonce@gmail.com>
2012-08-07 10:21:53 -0700

    updated django-respite, fixes #8

--- app/settings/default.py
+++ app/settings/default.py
@@ -83,8 +83,7 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    # @todo csrf breaks delete
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
