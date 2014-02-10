2b78450a3cac8cd9f0d770ee6b556dc43249f637
Felipe Reyes <freyes@tty.cl>
2014-02-10 12:14:29 -0300

    Added eb configuration

---
+++ app/settings/ebs.py
@@ -0,0 +1,17 @@
+import os
+from default import *
+
+DEBUG = bool(os.environ.get("DEBUG", ""))
+TEMPLATE_DEBUG = DEBUG
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
+    }
+}
